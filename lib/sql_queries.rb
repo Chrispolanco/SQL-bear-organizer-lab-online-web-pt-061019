2e119f76fc7d980be2b74872d01190ec768fdc4d
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-22 05:35:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "Select * From bears Where name =null;"
+  "Select * From bears Where name is null;"
 end
