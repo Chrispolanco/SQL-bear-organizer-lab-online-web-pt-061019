e860421e7a55abca10abcece8fcfaa133015ae15
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-22 05:27:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Select color, count(color) From bears Order by count(color) limit 1"
+  "Select color, count(color) From bears Order by count(color) Asc limit 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
